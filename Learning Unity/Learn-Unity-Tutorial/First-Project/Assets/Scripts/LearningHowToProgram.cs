4a52df7124df3d04564cc4c09ac236173924938b
LukeBurnss <lukeburns115@gmail.com>
2025-01-23 01:06:09 +0000

    This aint gonna work is it

---
+++ Learning Unity/Learn-Unity-Tutorial/First-Project/Assets/Scripts/LearningHowToProgram.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningHowToProgram : MonoBehaviour
+{
+
+    float speed = 1.4f;
+
+    double mana = 15.5;
+
+    int health = 100;
+
+    string playerName = "Warrior";
+
+    bool isDead = false;
+
+    char oneChar = 'a';
+
+} // class
